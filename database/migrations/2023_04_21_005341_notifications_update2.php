eb02f39b7b29c89ec51be96a99de53147a26bbc8
tudorcrisan1231 <crisantudor79@yahoo.com>
2023-04-21 03:57:56 +0300

    notification migration

---
+++ database/migrations/2023_04_21_005341_notifications_update2.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+         
+            $table->string('type');
+            $table->string('sender_id');
+            $table->string('receiver_id');
+            $table->string('message');
+            $table->string('extra_data');
+            $table->text('data');
+       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            //
+        });
+    }
+};
